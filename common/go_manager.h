5151b7eda6e5b501a784df1e09c9f8cdc43eaed3
Sean Leonard <seanleonard1@gmail.com>
2014-02-11 20:48:37 +0000

    -Create a new class ComponentTypeDatabase, which stores information about the various Component types including their name and which type they descend from. -Add a function ComponentTypeDatabase::descendant to perform an IsA query given a subtype and a supertype.
    SVN r4562 (net3-base)

--- common/go_manager.h
+++ common/go_manager.h
@@ -230,4 +230,46 @@ private:
 	ComponentTypeStore			mComponentTypes;
 };
 
+
+
+class ComponentTypeDatabase
+{
+public:
+	ComponentTypeDatabase();
+	~ComponentTypeDatabase();
+
+	void registerType(const OString& type_name, const OString& type_parent_name, const Component& prototype);
+	void unregisterType(const OString& type_name);
+	void clearTypes();
+
+	Component* buildComponent(const OString& type_name) const;
+
+	bool descendant(const OString& type_name, const OString& type_parent_name) const;
+
+private:
+
+	struct ComponentTypeRecord
+	{
+		ComponentTypeRecord() :
+			mPrototype(NULL), mIsComposite(false)
+		{ }
+
+		ComponentTypeRecord(const OString& type_name, const OString& type_parent_name, const Component& prototype) :
+			mTypeName(type_name), mTypeParentName(type_parent_name),
+			mPrototype(prototype.clone()), mIsComposite(prototype.isComposite())
+		{ }
+
+		OString				mTypeName;
+		OString				mTypeParentName;
+
+		const Component*	mPrototype;
+		bool				mIsComposite;
+	};
+
+
+	typedef OHashTable<OString, ComponentTypeRecord> TypeRecordTable;
+	TypeRecordTable			mTypes;
+};
+
+
 #endif	// __GO_MANAGER_H__
